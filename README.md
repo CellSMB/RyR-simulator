a0c709aa11ff370ebbb1799bcb723bb1f28502fd
Vijay Rajagopal <vijayaraghavan@smart.mit.edu>
2013-04-25 00:37:45 +0800

    First code submission

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 ryr-simulator
 =============
+An R-statistics package script for simulating anatomically realistic distributions of RyR clusters around mitochondria and the contractile machinery. 
+
+**Applications/Packages required:**
+
+R-statistics package available from www.r-project.org
+MATLAB      
+
+**FOLDER INFORMATION**
+input-files/: 
+
+master-cell/
+
+ - directory consisting of all the input files necessary for generating statistics on nearest neighborhood distances between RyR clusters and axial and radial distances of RyR clusters from the z-disc. Folder currently contains data extracted from Cell 1 of submitted paper.
+
+target-cell/
+
+ - directory of same input files as those in master-cell but with information from target cell onto which RyR clusters must be simulated. Folder currently contains data extracted from Cell 1 of submitted paper.
+
+output-files/:
+
+target-cell/
+
+ - The RyR cluster patterns simulated on the target cell along with the histograms of the observed ryr cluster nearest-neighborhood distances and the axial and radial distances to the z-discs. Current simulations reconstruct RyR cluster distributions on Cell 1 from the observed data on Cell 1 from submitted paper.
+
+source:
+
+contains all the source files for running in R.
+
+*ryr-simulator.R*: The main program to run the RyR cluster simulator. 
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+NOTE: CHANGE PATH TO LOCAL MACHINE PATH OF THE ryr-simulator/source DIRECTORY ON LINE NUMBER 28
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+*nnd-calculators.R*: A bunch of calculators of nearest-neighborhood distances; currently using CGW version which measures torus distance. 
---
+++ source/nnd-calculators.R
@@ -0,0 +1,86 @@
+# Written by Evan I. Blumgart 30th Nov. 2010
+#Further modified by Cameron Walker, Vijay Rajagopal
+# This function simulates 3D spatial point process data within a sampling brick
+# according to the observed intensity and covariates. In this case, the point
+# process represents Ryanodine Receptors (RyR's) in rat ventricular muscle,
+# where the covariate is the locations of Z-disks. Contraints on nearest
+# neighbour distances is also enforced. Edge correction is not implemented in
+# the algorithm, thus the input data must have a buffer zone.
+#
+# Inputs: x, y, z - Cartesian coordinates of events after edge correction
+#         xl, xu, yl, yu, zl, zu - dimensions of sampling brick
+#         n - event count
+#         R - data frame containing coordinates of all events inside brick
+#         X - data frame containing coordinates of all events
+#         W - data frame containing coordinates of all           # non-myofibril voxels
+#	    d - distance function
+
+# calculates observed nearest-neighbours
+findObsNNDist <- function(R,X) {  ##removed M,xl,xu,yl,yu,zl,zu
+obsNNDist <- c()
+for (i in 1:length(R$x)) {  
+  cat('point',i,"\n")                                    ##was 1 to n, should be 1:length(R$x) ?
+  obsNNcur <- 1000
+  for (j in 1:length(X$x)) {
+    if ((R$x[i] != X$x[j]) | (R$y[i] != X$y[j]) | (R$z[i] != X$z[j])) { # exclude NN of length 0 - CGW: changed && to |
+      obsNN <- sqrt((R$x[i]-X$x[j])^2 + (R$y[i]-X$y[j])^2 + (R$z[i]-X$z[j])^2)
+      if (obsNN < obsNNcur) {
+        obsNNcur <- obsNN
+      }
+    }
+  }
+  obsNNDist[i] <- obsNNcur
+}
+return(obsNNDist)
+}
+
+findObsNNDist_CGWold <- function(R,X,l,u) { #Torus edge corrected
+  apply(outer(1:dim(R)[1],1:dim(X)[1],function(r,x){
+      sqrt( (R[r,1]-X[x,1])^2+
+            (R[r,2]-X[x,2])^2+
+            (R[r,3]-X[x,3])^2)}),
+          1,function(x){min(x[x>0])})
+}
+
+findObsNNDist_CGW <- function(R,X,l,u) { #Torus edge corrected
+  apply(outer(1:(dim(R)[1]),1:(dim(X)[1]),function(r,x){
+      sqrt( (pmin(abs(R[r,1]-X[x,1]),u[1]-l[1]-abs(R[r,1]-X[x,1])))^2+
+            (pmin(abs(R[r,2]-X[x,2]),u[2]-l[2]-abs(R[r,2]-X[x,2])))^2+
+            (pmin(abs(R[r,3]-X[x,3]),u[3]-l[3]-abs(R[r,3]-X[x,3])))^2)}),
+          1,function(x){min(x[x>0])})
+}
+
+findWhichObsNNDist_CGW <- function(R,X,l,u) {
+  apply(outer(1:dim(R)[1],1:dim(X)[1],function(r,x){
+      sqrt( (pmin(abs(R[r,1]-X[x,1]),u[1]-l[1]-abs(R[r,1]-X[x,1])))^2+
+            (pmin(abs(R[r,2]-X[x,2]),u[2]-l[2]-abs(R[r,2]-X[x,2])))^2+
+            (pmin(abs(R[r,3]-X[x,3]),u[3]-l[3]-abs(R[r,3]-X[x,3])))^2)}),
+          1,function(x){which(x==min(x[x>0]))})
+}
+
+findDist_CGW <- function(R,X) {
+  out=apply(outer(1:dim(R)[1],1:dim(X)[1],function(r,x){sqrt((R[r,1]-X[x,1])^2+(R[r,2]-X[x,2])^2+(R[r,3]-X[x,3])^2)}),
+          1,function(x){
+         nnD=min(x[x>0]);nnInd=which(x==nnD);c(nnD,nnInd)})
+}
+
+findDist <- function(R,X) { #CGW - speed up the simulation by identifying legal points
+  outer(1:dim(R)[1],1:dim(X)[1],function(r,x){sqrt((R[r,1]-X[x,1])^2+(R[r,2]-X[x,2])^2+(R[r,3]-X[x,3])^2)})
+}
+
+# find nearest neighbour distance for proposal event
+findNNDist <- function(xp, yp, zp, simRyRx, simRyRy, simRyRz) {
+  NNcur = 1000
+  whichNN <- 0
+  for (j in 1:length(simRyRx)) {
+    NN <- sqrt((xp-simRyRx[j])^2 + (yp-simRyRy[j])^2 + (zp-simRyRz[j])^2)
+    if (NN < NNcur) {
+      whichNN <- j
+      NNcur = NN
+    }
+  }
+  return(c(NNcur,whichNN))
+}
+
+# simulateRyR <- function(x,y,z,n,xl,xu,yl,yu,zl,zu,X,M) {
+
