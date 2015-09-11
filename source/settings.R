013c01a7d6958d6e1fe84c24e2164747217ca405
Vijay Rajagopal <vijay.rajagopal@unimelb.edu.au>
2015-09-11 11:42:08 +1000

    Fixing paths for repo demo
    replaced personal machine folder address with github demo folder
    structure references

--- source/settings.R
+++ source/settings.R
@@ -2,11 +2,11 @@
 #path of source files
 #setwd("/Users/vraj004/ryr-simulator/source")
 #path of master geometry
-path2="/Users/vrajagopal/Documents/heart/sims/R/camSim/sham11/Cell1/SR_width_6/"
+path2="/../input-files/master-cell/"
 #path of target geometry
-path4="/Users/vrajagopal/Documents/heart/sims/R/camSim/schneider_tomo_sim/high-res-ryrsim-inputs/"
+path4="/../input-files/target-cell/"
 #path of output files
-path3="/Users/vrajagopal/Documents/heart/sims/R/camSim/schneider_tomo_sim/high-res-ryrsim-inputs/fixedNND/"
+path3="/../output-files/target-cell/"
 N=123
 etol = 0.005
 numIter = 2000000
